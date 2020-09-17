54d3397e54de70fc018d2800117b1db27c52bbb4
Robert Balonek <robertbalonek@gmail.com>
2020-09-16 21:47:39 -0400

    Finiehsed User Auth Backend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,6 +46,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:username, :user_img, :bio, :email, :password_digest, :role_id)
+      params.require(:user).permit(:username, :user_img, :bio, :email, 'password')
     end
 end
