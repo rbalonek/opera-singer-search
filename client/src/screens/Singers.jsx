645a8be76a9d1621f672d5672e7b9609e4eaefb2
Robert Balonek <robertbalonek@gmail.com>
2020-09-17 20:30:22 -0400

    Show Roles for SingerDetail

--- client/src/screens/Singers.jsx
+++ client/src/screens/Singers.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 export default function Singers(props) {
   const { singers } = props;
 
-console.log(singers)
+// console.log(singers)
 
   return (
     <div>
