0bf16e3bd1b6990909b79ff5c9c9236cf2cdf774
Robert Balonek <robertbalonek@Roberts-MBP.attlocal.net>
2020-11-05 11:22:23 -0800

    Header Responsive

--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "./css/Login.css";
 
 export default function Login(props) {
   const [formData, setFormData] = useState({
@@ -17,41 +18,50 @@ export default function Login(props) {
   };
 
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        props.loginSubmit(formData);
-        //Could put history push here,
-      }}
-    >
-      <h3>Login</h3>
-      <label>
-        Username:
-        <input
-          type="text"
-          name="username"
-          value={username}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-      <br />
-      <label>
-        Password:
-        <input
-          type="text"
-          name="password"
-          value={password}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-      <br />
-      <Link to="/register">Register</Link>
-      <br />
-      <br />
-      <br />
-      <button>Submit</button>
-    </form>
+    <div className="form-div">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          props.loginSubmit(formData);
+          //Could put history push here,
+        }}
+      >
+        <h3>Login</h3>
+        <div className="label-div">
+          <label>
+            <input
+              placeholder="User name"
+              className="input"
+              type="text"
+              name="username"
+              value={username}
+              onChange={handleChange}
+            />
+          </label>
+          <br />
+          <br />
+          <label>
+            <input
+              placeholder="Password"
+              className="input"
+              type="text"
+              name="password"
+              value={password}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+        <br />
+        <br />
+        <button className="submit-button">Submit</button>
+        <br />
+        <br />
+        <p>OR</p>
+        <br />
+        <Link to="/register">
+          <div className="register-button">Register</div>
+        </Link>
+      </form>
+    </div>
   );
 }
